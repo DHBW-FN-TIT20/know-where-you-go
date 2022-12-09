157ef10e981eec0466ce1073157049b0d306cba1
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2022-12-09 10:07:59 +0000

    changed tab to 2 spaces

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,169 +1,169 @@
 import React, { useState } from "react";
 import process from "node:process";
 import {
-    f7,
-    f7ready,
-    App,
-    Panel,
-    Views,
-    View,
-    Popup,
-    Page,
-    Navbar,
-    Toolbar,
-    NavRight,
-    Link,
-    Block,
-    LoginScreen,
-    LoginScreenTitle,
-    List,
-    ListInput,
-    ListButton,
-    BlockFooter,
+  f7,
+  f7ready,
+  App,
+  Panel,
+  Views,
+  View,
+  Popup,
+  Page,
+  Navbar,
+  Toolbar,
+  NavRight,
+  Link,
+  Block,
+  LoginScreen,
+  LoginScreenTitle,
+  List,
+  ListInput,
+  ListButton,
+  BlockFooter,
 } from "framework7-react";
 
 import routes from "../js/routes";
 import store from "../js/store";
 
 const MyApp = () => {
-    // Login screen demo data
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  // Login screen demo data
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    // Framework7 Parameters
-    const f7params = {
-        name: "Know Where You Go", // App name
-        theme: "auto", // Automatic theme detection
+  // Framework7 Parameters
+  const f7params = {
+    name: "Know Where You Go", // App name
+    theme: "auto", // Automatic theme detection
 
-        // App store
-        store: store,
-        // App routes
-        routes: routes,
-        // Register service worker (only on production build)
-        serviceWorker:
-            process.env.NODE_ENV === "production"
-                ? {
-                      path: "/service-worker.js",
-                  }
-                : {},
-    };
-    const alertLoginData = () => {
-        f7.dialog.alert("Username: " + username + "<br>Password: " + password, () => {
-            f7.loginScreen.close();
-        });
-    };
-    f7ready(() => {
-        // Call F7 APIs here
+    // App store
+    store: store,
+    // App routes
+    routes: routes,
+    // Register service worker (only on production build)
+    serviceWorker:
+      process.env.NODE_ENV === "production"
+        ? {
+            path: "/service-worker.js",
+          }
+        : {},
+  };
+  const alertLoginData = () => {
+    f7.dialog.alert("Username: " + username + "<br>Password: " + password, () => {
+      f7.loginScreen.close();
     });
+  };
+  f7ready(() => {
+    // Call F7 APIs here
+  });
 
-    return (
-        <App {...f7params} dark>
-            {/* Left panel with cover effect*/}
-            <Panel left cover dark>
-                <View>
-                    <Page>
-                        <Navbar title="Left Panel" />
-                        <Block>Left panel content goes here</Block>
-                    </Page>
-                </View>
-            </Panel>
+  return (
+    <App {...f7params} dark>
+      {/* Left panel with cover effect*/}
+      <Panel left cover dark>
+        <View>
+          <Page>
+            <Navbar title="Left Panel" />
+            <Block>Left panel content goes here</Block>
+          </Page>
+        </View>
+      </Panel>
 
-            {/* Right panel with reveal effect*/}
-            <Panel right reveal dark>
-                <View>
-                    <Page>
-                        <Navbar title="Right Panel" />
-                        <Block>Right panel content goes here</Block>
-                    </Page>
-                </View>
-            </Panel>
+      {/* Right panel with reveal effect*/}
+      <Panel right reveal dark>
+        <View>
+          <Page>
+            <Navbar title="Right Panel" />
+            <Block>Right panel content goes here</Block>
+          </Page>
+        </View>
+      </Panel>
 
-            {/* Views/Tabs container */}
-            <Views tabs className="safe-areas">
-                {/* Tabbar for switching views-tabs */}
-                <Toolbar tabbar labels bottom>
-                    <Link
-                        tabLink="#view-home"
-                        tabLinkActive
-                        iconIos="f7:house_fill"
-                        iconAurora="f7:house_fill"
-                        iconMd="material:home"
-                        text="Home"
-                    />
-                    <Link
-                        tabLink="#view-catalog"
-                        iconIos="f7:square_list_fill"
-                        iconAurora="f7:square_list_fill"
-                        iconMd="material:view_list"
-                        text="Catalog"
-                    />
-                    <Link
-                        tabLink="#view-settings"
-                        iconIos="f7:gear"
-                        iconAurora="f7:gear"
-                        iconMd="material:settings"
-                        text="Settings"
-                    />
-                </Toolbar>
+      {/* Views/Tabs container */}
+      <Views tabs className="safe-areas">
+        {/* Tabbar for switching views-tabs */}
+        <Toolbar tabbar labels bottom>
+          <Link
+            tabLink="#view-home"
+            tabLinkActive
+            iconIos="f7:house_fill"
+            iconAurora="f7:house_fill"
+            iconMd="material:home"
+            text="Home"
+          />
+          <Link
+            tabLink="#view-catalog"
+            iconIos="f7:square_list_fill"
+            iconAurora="f7:square_list_fill"
+            iconMd="material:view_list"
+            text="Catalog"
+          />
+          <Link
+            tabLink="#view-settings"
+            iconIos="f7:gear"
+            iconAurora="f7:gear"
+            iconMd="material:settings"
+            text="Settings"
+          />
+        </Toolbar>
 
-                {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */}
-                <View id="view-home" main tab tabActive url="/" />
+        {/* Your main view/tab, should have "view-main" class. It also has "tabActive" prop */}
+        <View id="view-home" main tab tabActive url="/" />
 
-                {/* Catalog View */}
-                <View id="view-catalog" name="catalog" tab url="/catalog/" />
+        {/* Catalog View */}
+        <View id="view-catalog" name="catalog" tab url="/catalog/" />
 
-                {/* Settings View */}
-                <View id="view-settings" name="settings" tab url="/settings/" />
-            </Views>
+        {/* Settings View */}
+        <View id="view-settings" name="settings" tab url="/settings/" />
+      </Views>
 
-            {/* Popup */}
-            <Popup id="my-popup">
-                <View>
-                    <Page>
-                        <Navbar title="Popup">
-                            <NavRight>
-                                <Link popupClose>Close</Link>
-                            </NavRight>
-                        </Navbar>
-                        <Block>
-                            <p>Popup content goes here.</p>
-                        </Block>
-                    </Page>
-                </View>
-            </Popup>
+      {/* Popup */}
+      <Popup id="my-popup">
+        <View>
+          <Page>
+            <Navbar title="Popup">
+              <NavRight>
+                <Link popupClose>Close</Link>
+              </NavRight>
+            </Navbar>
+            <Block>
+              <p>Popup content goes here.</p>
+            </Block>
+          </Page>
+        </View>
+      </Popup>
 
-            <LoginScreen id="my-login-screen">
-                <View>
-                    <Page loginScreen>
-                        <LoginScreenTitle>Login</LoginScreenTitle>
-                        <List form>
-                            <ListInput
-                                type="text"
-                                name="username"
-                                placeholder="Your username"
-                                value={username}
-                                onInput={e => setUsername(e.target.value)}
-                            ></ListInput>
-                            <ListInput
-                                type="password"
-                                name="password"
-                                placeholder="Your password"
-                                value={password}
-                                onInput={e => setPassword(e.target.value)}
-                            ></ListInput>
-                        </List>
-                        <List>
-                            <ListButton title="Sign In" onClick={() => alertLoginData()} />
-                            <BlockFooter>
-                                Some text about login information.
-                                <br />
-                                Click &quot;Sign In&quot; to close Login Screen
-                            </BlockFooter>
-                        </List>
-                    </Page>
-                </View>
-            </LoginScreen>
-        </App>
-    );
+      <LoginScreen id="my-login-screen">
+        <View>
+          <Page loginScreen>
+            <LoginScreenTitle>Login</LoginScreenTitle>
+            <List form>
+              <ListInput
+                type="text"
+                name="username"
+                placeholder="Your username"
+                value={username}
+                onInput={e => setUsername(e.target.value)}
+              ></ListInput>
+              <ListInput
+                type="password"
+                name="password"
+                placeholder="Your password"
+                value={password}
+                onInput={e => setPassword(e.target.value)}
+              ></ListInput>
+            </List>
+            <List>
+              <ListButton title="Sign In" onClick={() => alertLoginData()} />
+              <BlockFooter>
+                Some text about login information.
+                <br />
+                Click &quot;Sign In&quot; to close Login Screen
+              </BlockFooter>
+            </List>
+          </Page>
+        </View>
+      </LoginScreen>
+    </App>
+  );
 };
 export default MyApp;
--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -3,33 +3,33 @@ import { Page, Navbar, List, ListItem, Block, Button, useStore } from "framework
 import store from "../js/store";
 
 const CatalogPage = () => {
-    const products = useStore("products");
+  const products = useStore("products");
 
-    const addProduct = () => {
-        store.dispatch("addProduct", {
-            id: "4",
-            title: "Apple iPhone 12",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-        });
-    };
+  const addProduct = () => {
+    store.dispatch("addProduct", {
+      id: "4",
+      title: "Apple iPhone 12",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    });
+  };
 
-    return (
-        <Page name="catalog">
-            <Navbar title="Catalog" />
-            <List>
-                {products.map(product => (
-                    <ListItem key={product.id} title={product.title} link={`/product/${product.id}/`} />
-                ))}
-            </List>
-            {products.length === 3 && (
-                <Block>
-                    <Button fill onClick={addProduct}>
-                        Add Product
-                    </Button>
-                </Block>
-            )}
-        </Page>
-    );
+  return (
+    <Page name="catalog">
+      <Navbar title="Catalog" />
+      <List>
+        {products.map(product => (
+          <ListItem key={product.id} title={product.title} link={`/product/${product.id}/`} />
+        ))}
+      </List>
+      {products.length === 3 && (
+        <Block>
+          <Button fill onClick={addProduct}>
+            Add Product
+          </Button>
+        </Block>
+      )}
+    </Page>
+  );
 };
 
 export default CatalogPage;
