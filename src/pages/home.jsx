c54d1ddf7fac04fbec6789359782ea8ca63726fa
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2022-12-12 17:49:01 +0100

    show leaflet map

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { Page, Navbar, NavLeft, Link } from "framework7-react";
+import { Page } from "framework7-react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 class Home extends React.Component {
+  componentDidMount() {
+    // fire a resize event to fix leaflets map sizing bug
+    window.dispatchEvent(new Event("resize"));
+  }
+
   render() {
     return (
       <Page name="home">
-        {/* Top Navbar */}
-
-        <Navbar sliding={false}>
-          <NavLeft>
-            <Link iconIos="f7:menu" iconAurora="f7:menu" iconMd="material:menu" panelOpen="left" />
-          </NavLeft>
-        </Navbar>
-
-        {/* Page content */}
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: "100%" }}
+          touchZoom={true}
+          zoomControl={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </Page>
     );
   }
