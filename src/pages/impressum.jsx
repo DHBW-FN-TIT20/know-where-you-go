f8f38e4d69de14a7c6f15cfdc977aa3ad4bc4110
baldur <baldur@siegels.us>
2023-01-19 12:25:36 +0100

    removed urheberrecht section

--- src/pages/impressum.jsx
+++ src/pages/impressum.jsx
@@ -128,17 +128,6 @@ class Impressum extends React.Component {
             jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von
             Rechtsverletzungen werden wir derartige Links umgehend entfernen.
           </p>
-          <h3>Urheberrecht</h3>
-          <p>
-            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
-            Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
-            Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
-            Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit
-            die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.
-            Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
-            Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von
-            Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
-          </p>
         </Block>
         <Block className="font-light text-center">&#169; FKW Software Solutions, 2022</Block>
       </Page>
